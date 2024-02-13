3cb38b57e81fdc0f29f2a589ecba1bfd4dfe8a00
Ameer Ameen <ameerapex496@gmail.com>
2024-02-13 03:22:57 +0100

    added cards file with data in it

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
-
+import Card from './Card'
 const Cards = () => {
-  return <div>Cards</div>
+  const arryObj = [
+    {
+      img: 'https://images.unsplash.com/photo-15043494',
+      title: 'Ameer',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1503494',
+      title: 'Ahmed',
+    },
+    {
+      img: 'https://images.unsplash.com/photo-15042494',
+      title: 'Ali',
+    },
+  ]
+
+  return (
+    <>
+      {arryObj.map((item, index) => (
+        <Card key={index} title={item.title} img={item.img} />
+      ))}
+    </>
+  )
 }
 
 export default Cards
